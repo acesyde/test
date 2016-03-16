7450b3d11394265847478e7f89a772b87003cb33
Ace <acesyde@gmail.com>
2016-03-16 22:22:59 +0100

    Unit Tests

---
+++ ConsoleApplication1/ConsoleApplication1/Models/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication1.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string SayHello()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName))
+                return "Hello Anonymous";
+
+            return $"Hello {LastName} {FirstName}";
+        }
+
+        public string GenerateEmail()
+        {
+            if (string.IsNullOrWhiteSpace(FirstName) || string.IsNullOrWhiteSpace(LastName))
+                throw new ArgumentNullException("FirstName or LastName cannot be null or empty");
+
+            return $"{FirstName}.{LastName}@orange.com".ToLower();
+        }
+    }
+}
---
+++ ConsoleApplication1/UnitTestProject/Models/PersonUnitTest.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApplication1.Models;
+using System;
+using Xunit;
+
+namespace UnitTestProject.Models
+{
+    /// <summary>
+    /// Summary description for PersonUnitTest
+    /// </summary>
+    public class PersonUnitTest
+    {
+        [Theory]
+        [InlineData("Mercier", "Pierre", "Hello Mercier Pierre")]
+        [InlineData(null, null, "Hello Anonymous")]
+        [InlineData("", "", "Hello Anonymous")]
+        [InlineData(" ", " ", "Hello Anonymous")]
+        public void SayHello_Should_ReturnCorrectly(string lastname, string firstname, string expectedResult)
+        {
+            var person = new Person { LastName = lastname, FirstName = firstname };
+            string result = person.SayHello();
+            Assert.Equal<string>(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("Mercier", "Pierre", "pierre.mercier@orange.com")]
+        public void GenerateEmail_Should_ReturnCorrectly(string lastname, string firstname, string expectedResult)
+        {
+            var person = new Person { LastName = lastname, FirstName = firstname };
+            string result = person.GenerateEmail();
+            Assert.Equal<string>(expectedResult, result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData(null, null)]
+        [InlineData(" ", " ")]
+        public void GenerateEmail_ShouldThrowArgumentNullException(string lastname, string firstname)
+        {
+            var person = new Person { LastName = lastname, FirstName = firstname };
+            Assert.Throws<ArgumentNullException>(() => person.GenerateEmail());
+        }
+    }
+}
